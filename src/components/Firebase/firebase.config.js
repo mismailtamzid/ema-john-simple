6900e2c759e60047c32ebd7525f5b51952b04b5b
mismailtamzid <mismailtamzid@gmail.com>
2021-10-13 22:55:39 +0600

    first commit

---
+++ src/components/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD8hxuUE--myaKSuNkWSteST0MXFHQiB0I",
+    authDomain: "ema-john-simple-cdd81.firebaseapp.com",
+    projectId: "ema-john-simple-cdd81",
+    storageBucket: "ema-john-simple-cdd81.appspot.com",
+    messagingSenderId: "1026686781559",
+    appId: "1:1026686781559:web:074778282ce519650cde72",
+    measurementId: "G-SCHFCQY3CL"
+  };
+
+  export default firebaseConfig;
